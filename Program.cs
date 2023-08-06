ce7a2dece96643fe208884c6110a7c0e4d48be6c
«Magomed» <magomed@example.com>
2023-08-06 14:59:18 +0300

    все

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите два числа ");
+//Пользователь вел число 
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine(num1  + "больше");
+}
+ else if (num1 < num2)
+ {
+    System.Console.WriteLine(num2  +  "больше");
+ }
